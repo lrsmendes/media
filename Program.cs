f726b7ae845570a9de0330db1371890c88805e36
Lucas Raziel <101843880+lrsmendes@users.noreply.github.com>
2022-03-17 21:18:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(c)Raziel, 2022\n");
+            Double a, b, c, med;
+            Console.Write("Escreva um valor: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nEscreva um valor: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nEscreva um valor: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            med = (a + b + c) / 3;
+            Console.WriteLine("\nA sua média aritimetica é de: " + med);
+            Console.Write("\nAperte uma tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
